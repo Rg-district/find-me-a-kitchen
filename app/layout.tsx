e2f2402612e1cebbc8521076954ea3a9373c826f
Dopamine District <dopaminedistrict@Dopamines-MacBook-Pro.local>
2026-03-09 20:48:20 +0000

    feat: initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,10 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Inter, Geist } from 'next/font/google'
 import './globals.css'
 import { headers } from 'next/headers'
+import { cn } from "@/lib/utils";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
 
 const inter = Inter({ subsets: ['latin'], weight: ['300','400','500','600','700','800'] })
 
@@ -15,7 +18,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" className={cn("font-sans", geist.variable)}>
       <body className={inter.className}>
         {children}
         <footer style={{
